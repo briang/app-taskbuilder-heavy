0cf0f8047e866116cb96655702cc0787ade245c7
brian greenfield <briang@cpan.org>
2011-08-22 23:44:36 +0100

    add ATB::Heavy.pm shell

---
+++ lib/App/TaskBuilder/Heavy.pm
@@ -0,0 +1,15 @@
+package App::TaskBuilder::Heavy;
+
+use 5.8.3;
+
+use strict;
+use warnings FATAL => 'all';
+#use diagnostics;
+
+#use Data::Dump;
+#use Carp qw();
+#use Moose;
+
+our $VERSION = '0.01';
+
+1;
